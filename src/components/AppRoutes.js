2c8d501c481cce6d4fd9ff8b414461df655d542b
Kinark <igormarcossi@gmail.com>
2019-09-16 18:15:07 -0300

    Removed react-router-transition

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react'
-import { Route } from 'react-router-dom';
-import { AnimatedSwitch } from 'react-router-transition'
+import { Switch, Route } from 'react-router-dom';
 
 import Home from '~/views/Home';
 // import Component from '~/views/Component';
@@ -8,10 +7,10 @@ import Home from '~/views/Home';
 export default class AppRoutes extends PureComponent {
    render() {
       return (
-         <AnimatedSwitch atEnter={{ opacity: 0 }} atLeave={{ opacity: 0 }} atActive={{ opacity: 1 }} className="switch-wrapper">
+         <Switch>
             <Route exact path="/" component={Home} />
             {/* <Route exact path="/component" component={Component} /> */}
-         </AnimatedSwitch>
+         </Switch>
       )
    }
 }
