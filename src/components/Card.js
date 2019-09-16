1478168dfa557b5d367169b6b239631921b49465
Kinark <igormarcossi@gmail.com>
2019-09-16 20:12:57 -0300

    Added Card component

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+import colors from '~/constants/colors'
+
+const Card = styled.div`
+   background-color: #fff;
+   border-radius: 4px;
+   border: solid 1px ${colors.LIGHT_BLUE};
+`
+
+export default Card
