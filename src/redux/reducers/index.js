23e4e36645a49f2efbba19ab56ed38305a64a21d
Kinark <igormarcossi@gmail.com>
2019-09-16 19:09:59 -0300

    Fixed old entries

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,31 +1,9 @@
 import { combineReducers } from 'redux'
-import { LOGOFF } from '../actions/auth'
 
-import initialStore from '../store/initialStore'
+import sessionsLog from './sessionsLog'
 
-import settings from './settings'
-import app from './app'
-import data from './data'
-import auth from './auth'
-import myWorlds from './myWorlds'
-import whereILive from './whereILive'
-import toastNotifications from './toastNotifications'
-import myRulesets from './myRulesets'
-
-const appReducer = combineReducers({
-   myWorlds,
-   whereILive,
-   settings,
-   data,
-   auth,
-   app,
-   toastNotifications,
-   myRulesets
+const rootReducer = combineReducers({
+   sessionsLog
 })
-
-const rootReducer = (state, action) => {
-   if (action.type === LOGOFF) state = Object.assign(initialStore, { app: { connected: true } })
-   return appReducer(state, action)
-}
 
 export default rootReducer
