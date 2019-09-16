6ffd4ebf8ce3f27eae88c8754477217f5ed3c5c3
Kinark <igormarcossi@gmail.com>
2019-09-16 20:12:15 -0300

    Created Input component

---
+++ src/components/Input.js
@@ -0,0 +1,17 @@
+import styled, { css } from 'styled-components'
+
+import colors from '~/constants/colors';
+
+export const InputBase = css`
+   border-radius: 4px;
+   height: 24px;
+   border: solid 2px ${colors.DARK_BLUE};
+   margin: 1rem;
+`
+
+const Input = styled.input`
+   ${InputBase};
+   text-align: center;
+`
+
+export default Input
