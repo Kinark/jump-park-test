1c535e8eea1654c2edff17966eb8b7d42a027bc4
Kinark <igormarcossi@gmail.com>
2019-09-17 01:10:33 -0300

    Fixed alignment of ResponsiveImg

--- src/components/ResponsiveImg.js
+++ src/components/ResponsiveImg.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 const ResponsiveImg = styled.img`
    width: ${({ maxWidth }) => maxWidth}px;
    max-width: 100%;
+   display: block;
+   margin: auto;
 `
 
 export default ResponsiveImg
